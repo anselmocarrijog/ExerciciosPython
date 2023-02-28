0fe064f19be765897cfe69bc30af950920d82a65
anselmocarrijog <54807443+anselmocarrijog@users.noreply.github.com>
2023-02-28 17:09:21 -0300

    Atualização
    17:09

---
+++ EstruturaSequencial/Exercicio_03.py
@@ -0,0 +1,6 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+a = int(input('Digite o primeiro número:'))
+b = int(input('Digite o Segundo número:'))
+
+soma = a + b
+print('A soma é:',soma)
